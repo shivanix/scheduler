26e65df94466b6f2308688bd99a63d01856e978b
shivanix <xuxu@LAPTOP-9J0KT8BE.localdomain>
2021-08-02 18:19:04 -0400

    Now using classNames to controll class of the component; Using formnatSpots function to render the appropiate text based on spots remaining

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,27 @@
 import React from "react";
 
+import "components/DayListItem.scss";
+import classNames from 'classnames/bind';
+
 export default function DayListItem(props) {
+let dayClass = classNames('day-list__item' ,{"day-list__item": props.selected, "day-list__item--full": (props.spots===0) })
+
+
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
+
+}
+
+function formatSpots(spots){
+if(spots===0){
+return `no spots remaining`;
+}
+if(spots===1){
+  return `${spots} spot remaining`;
+}
+return `${spots} spots remaining`;
 }
