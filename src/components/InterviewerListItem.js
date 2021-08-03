d27f22c0faee813fb3fdb0822ff733e6d2a67a6e
shivanix <xuxu@LAPTOP-9J0KT8BE.localdomain>
2021-08-03 15:00:05 -0400

    finished building the componeny by using values from props in code

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 import "components/InterviewerListItem.scss";
+import classNames from 'classnames';
 
-export default function InterviewerListItem(){
+export default function InterviewerItem(props){
+let interviewerClass = classNames('interviewers__item', {'interviewers__item--selected': props.selected});
 
-  return (<li className="interviewers__item">
+
+  return (<li className={interviewerClass} onClick = {props.setInterviewer}>
   <img
     className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
+    src={props.avatar}
+    alt={props.name}
   />
-  Sylvia Palmer
+  {props.selected && props.name}
 </li>);
 }
