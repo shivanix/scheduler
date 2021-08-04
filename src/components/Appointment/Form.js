4f06fe919e2ff38ca1f0105deeaf443a85cf6b15
shivanix <xuxu@LAPTOP-9J0KT8BE.localdomain>
2021-08-04 18:44:53 -0400

    created Form comp; added stories for create and edit;added name & interviewer to state; set a default state;added cancel function that resets the form

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+
+import InterviewerList from "components/InterviewerList";
+
+import Button from "components/Button";
+
+export default function Form (props){
+  //  console.log("#FORM PROPS#", props);
+
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  
+  // console.log("ffffffffffffffffffffff", props.interviewer);
+
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  }
+
+  const Cancel = () => {
+    reset();
+    props.onCancel();
+
+  }
+  const onChangeName = (event) => {
+    // console.log("-------", event.target.value);
+    // console.log("----#####---", event.target);
+    setName(event.target.value)
+  } 
+
+  const onChangeInterviewer = (interviewer_id) => {
+     //console.log("interviewerrrrrrrrrrrrr", interviewer_id);
+  setInterviewer(interviewer_id)
+  }
+
+return (
+  <main className="appointment__card appointment__card--create">
+  <section className="appointment__card-left">
+
+    <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+      <input
+        className="appointment__create-input text--semi-bold"
+        value={name}
+      
+        onChange={onChangeName}
+        // onChange={(event) => {setName(event.target.value)}}
+        name="name"
+        type="text"
+        placeholder="Enter Student Name"
+        
+      />
+    </form>
+    <InterviewerList interviewers={props.interviewers} interviewer={interviewer} setInterviewer = {onChangeInterviewer} />
+  </section>
+
+  {/* 444-----  */}
+
+
+  <section className="appointment__card-right">
+    <section className="appointment__actions">
+      <Button danger onClick = {Cancel}>Cancel</Button>
+      <Button confirm onClick = {props.onSave}>Save</Button>
+    </section>
+  </section>
+</main>
+)
+}
