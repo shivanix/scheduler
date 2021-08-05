7bb27c11465df5906b105831fc9b17b92e1580d9
shivanix <xuxu@LAPTOP-9J0KT8BE.localdomain>
2021-08-04 21:18:52 -0400

    added test data; Mapped in Schedule section to create a list; spread the props; added last appointmnet to the end of the list

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,66 @@ import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 
+import Appointment from "./Appointment";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Zingiber officinalis",
+      interviewer:{
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png"
+      }
+    }
+  },
+
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Anton Van Leeuwenhoek",
+      interviewer:     {
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg"
+      }
+    }
+  },
+
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Schleiden",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg"
+      }
+    }
+  }
+];
+
 const days = [
   {
     id: 1,
@@ -46,7 +106,10 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map((appointment) => {
+          return(<Appointment key={appointment.id} {...appointment} />
+  )}
+  )}<Appointment key="last" time="5pm" />
       </section>
     </main>
   );
