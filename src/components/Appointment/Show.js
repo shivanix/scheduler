2f7b8298aed1f667d3239fbe586c13809c6ec72e
shivanix <xuxu@LAPTOP-9J0KT8BE.localdomain>
2021-08-04 21:05:20 -0400

    added console.log; spacing

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -1,17 +1,19 @@
 import React from "react";
 
 export default function Show(props){
-console.log("propssssssss", props.student);
+// console.log("propssssssss", props.student);
 
 
   return (<main className="appointment__card appointment__card--show">
   <section className="appointment__card-left">
     <h2 className="text--regular">{props.student}</h2>
+    
     <section className="interviewer">
       <h4 className="text--light">Interviewer</h4>
       <h3 className="text--regular">{props.interviewer.name}</h3>
     </section>
   </section>
+
   <section className="appointment__card-right">
     <section className="appointment__actions">
       <img
