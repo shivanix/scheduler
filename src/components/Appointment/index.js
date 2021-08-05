5f42d409c52a7a215a2d7877e8bc87352502a2c3
shivanix <xuxu@LAPTOP-9J0KT8BE.localdomain>
2021-08-04 21:08:04 -0400

    Appointmt comp renders Header taking time prop; used ternary operator to render show/empty based on props.interview

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 import"components/Appointment/styles.scss";
 
 export default function Appointment(props){
 
-  return (<article className="appointment"></article>)
+  // console.log("inreifeffffffffff", props.interview);
+
+  //Appointment component will only render the Show component if props.interview is truthy
+  return (<article className="appointment">
+    <Header time={props.time}/>
+    {props.interview ?  
+    <Show student = {props.interview.student}
+    interviewer = {props.interview.interviewer}/> : <Empty/>}
+    </article>)
 }
+
+
