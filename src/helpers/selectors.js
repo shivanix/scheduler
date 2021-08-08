092d75f567e52f6fdc43dbf732914d82e0f369b4
shivanix <xuxu@LAPTOP-9J0KT8BE.localdomain>
2021-08-08 19:20:31 -0400

    implemented getInterviewersForDay selector; And tests for its functionality

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -37,8 +37,8 @@ return filteredAppointments;
 /*------------------------------------------------------ */
 
 function getInterview(state, interview) {
-console.log("stateeeeeee", state);
-console.log("interviewObject", interview);  
+// console.log("stateeeeeee", state);
+// console.log("interviewObject", interview);  
 
 // console.log("dffffffffff", state.interviewers["1"]);
 
@@ -58,5 +58,39 @@ catch(err){
 }
 
 }
+/*-------------------------------------------------------------------------------- */
+function getInterviewersForDay(state, day) {
 
-export {getAppointmentsForDay, getInterview}
+  let filteredDays = [];
+  let filteredIDs = [];
+  let filteredAppointments = [];
+  
+  //Find the days that match with the desired day
+  for(let selectedDay of state.days){
+    if(selectedDay.name===day){
+      // console.log("selectedDay: ", selectedDay);
+      filteredDays.push(selectedDay);
+    }
+  }
+  
+  //Get the appointment ids from the filteredDays
+  for (const dayAppointmentObj of filteredDays) {
+    for(const apps of dayAppointmentObj.appointments){
+      filteredIDs.push(apps);
+    }
+  }
+  
+  //Get the appointment objects based on the desired ids
+  for (const id of filteredIDs) {
+    if(state.appointments[id]){
+      filteredAppointments.push(state.appointments[id]);
+    }
+  }
+  
+  // console.log("filteredDays: ", filteredDays);
+  // console.log("filteredIDs: ", filteredIDs);
+  // console.log("filteredAppointments: ", filteredAppointments);
+  return filteredAppointments;
+  }
+
+export {getAppointmentsForDay, getInterview, getInterviewersForDay}
