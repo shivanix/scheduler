01243036cf73d58925bb0eddca4469f8ef2891a4
shivanix <xuxu@LAPTOP-9J0KT8BE.localdomain>
2021-08-09 12:41:24 -0400

    corrected getInterviewersForDay function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -61,36 +61,11 @@ catch(err){
 /*-------------------------------------------------------------------------------- */
 function getInterviewersForDay(state, day) {
 
-  let filteredDays = [];
-  let filteredIDs = [];
-  let filteredAppointments = [];
-  
-  //Find the days that match with the desired day
-  for(let selectedDay of state.days){
-    if(selectedDay.name===day){
-      // console.log("selectedDay: ", selectedDay);
-      filteredDays.push(selectedDay);
-    }
-  }
-  
-  //Get the appointment ids from the filteredDays
-  for (const dayAppointmentObj of filteredDays) {
-    for(const apps of dayAppointmentObj.appointments){
-      filteredIDs.push(apps);
-    }
-  }
-  
-  //Get the appointment objects based on the desired ids
-  for (const id of filteredIDs) {
-    if(state.appointments[id]){
-      filteredAppointments.push(state.appointments[id]);
-    }
-  }
-  
-  // console.log("filteredDays: ", filteredDays);
-  // console.log("filteredIDs: ", filteredIDs);
-  // console.log("filteredAppointments: ", filteredAppointments);
-  return filteredAppointments;
+  const found = state.days.find(d => day === d.name);
+
+  if (state.days.length === 0 || found === undefined) return [];
+
+  return found.interviewers.map(id => state.interviewers[id]);
   }
 
 export {getAppointmentsForDay, getInterview, getInterviewersForDay}
