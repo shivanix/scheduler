bac77afad998355236cabee0bacc8196aefc2af8
shivanix <xuxu@LAPTOP-9J0KT8BE.localdomain>
2021-08-10 14:15:09 -0400

    created Appointment.test.js and tested the file component

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -12,7 +12,7 @@ const state = {
       id: 2,
       name: "Tuesday",
       appointments: [4, 5],
-      interviewers: [1,2]
+      // interviewers: [1,2]
     }
   ],
   appointments: {
@@ -97,27 +97,27 @@ test("getInterview returns null if no interview is booked", () => {
 /*----------------------------------------------------------------- */
 
 test("getInterviewersForDay returns an array", () => {
-  const result = getInterviewersForDay(state, "Monday");
+  const result = getInterviewersForDay(state, 1);
   expect(Array.isArray(result)).toBe(true);
 });
 
 test("getInterviewersForDay returns an array with a length matching the number of interviewers for that day", () => {
-  const result = getInterviewersForDay(state, "Monday");
-  expect(result.length).toEqual(3);
+  const result = getInterviewersForDay(state, 1);
+  expect(result.length).toEqual(2);
 });
 
 test("getInterviewersForDay returns an array containing the correct interviewer objects", () => {
-  const [first, second] = getInterviewersForDay(state, "Tuesday");
-  expect(first).toEqual(state.appointments["4"]);
-  expect(second).toEqual(state.appointments["5"]);
+  const [first, second] = getInterviewersForDay(state, 1);
+  expect(first).toEqual(state.appointments["1"]);
+  expect(second).toEqual(state.appointments["2"]);
 });
 
 test("getInterviewersForDay returns an empty array when the days data is empty", () => {
-  const result = getInterviewersForDay({ days: [] }, "Monday");
+  const result = getInterviewersForDay({ days: [] }, 2);
   expect(result.length).toEqual(0);
 });
 
 test("getInterviewersForDay returns an empty array when the day is not found", () => {
-  const result = getInterviewersForDay(state, "Wednesday");
+  const result = getInterviewersForDay(state, 3);
   expect(result.length).toEqual(0);
 });
