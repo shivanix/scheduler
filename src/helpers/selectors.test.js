1d4eafe3cf5bcdc315685ed8a1bbf2b7e21dab9f
shivanix <xuxu@LAPTOP-9J0KT8BE.localdomain>
2021-08-10 16:46:03 -0400

    added Form.js unit tests

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -12,7 +12,7 @@ const state = {
       id: 2,
       name: "Tuesday",
       appointments: [4, 5],
-      // interviewers: [1,2]
+       interviewers: [1,2]
     }
   ],
   appointments: {
@@ -94,30 +94,30 @@ test("getInterview returns null if no interview is booked", () => {
 });
 
 
-/*----------------------------------------------------------------- */
+/*--------------------------------getInterviewersForDay tests--------------------------------- */
 
 test("getInterviewersForDay returns an array", () => {
-  const result = getInterviewersForDay(state, 1);
+  const result = getInterviewersForDay(state, "Monday");
   expect(Array.isArray(result)).toBe(true);
 });
 
 test("getInterviewersForDay returns an array with a length matching the number of interviewers for that day", () => {
-  const result = getInterviewersForDay(state, 1);
+  const result = getInterviewersForDay(state, "Monday");
   expect(result.length).toEqual(2);
 });
 
 test("getInterviewersForDay returns an array containing the correct interviewer objects", () => {
-  const [first, second] = getInterviewersForDay(state, 1);
-  expect(first).toEqual(state.appointments["1"]);
-  expect(second).toEqual(state.appointments["2"]);
+  const [first, second] = getInterviewersForDay(state, "Tuesday");
+  expect(first).toEqual(state.interviewers["1"]);
+  expect(second).toEqual(state.interviewers["2"]);
 });
 
 test("getInterviewersForDay returns an empty array when the days data is empty", () => {
-  const result = getInterviewersForDay({ days: [] }, 2);
+  const result = getInterviewersForDay({ days: [] }, "Monday");
   expect(result.length).toEqual(0);
 });
 
 test("getInterviewersForDay returns an empty array when the day is not found", () => {
-  const result = getInterviewersForDay(state, 3);
+  const result = getInterviewersForDay(state, "Wednesday");
   expect(result.length).toEqual(0);
 });
