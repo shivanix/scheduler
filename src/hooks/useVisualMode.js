f41978245a22cd410ea0dbf270d3dbe62d67328e
shivanix <xuxu@LAPTOP-9J0KT8BE.localdomain>
2021-08-09 21:25:50 -0400

    Used setHistory to set state in useVisualMode hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,24 +5,27 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition (newMode, replace = false) {
+function transition (newMode, replace = false) {
 
   setMode(newMode);   
+  
   if (replace) {
     history[history.length-1] = newMode
-  }
-  else{
+  } else { 
     history.push(newMode);
   }
+  setHistory([ ...history]);
 }
 
-  function back() {
+function back() {
     if(history.length > 1){
       history.pop();
 
       setMode(history[history.length-1]);
+      setHistory([ ...history]);
     }
-    
-  }
+}
+  console.log("HISTORYYYY", history);
+
   return {mode, transition, back};
 }
