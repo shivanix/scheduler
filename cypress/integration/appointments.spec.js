b4fc2b9cb63e3408d3e9bc1b3d8a744e648e053a
shivanix <xuxu@LAPTOP-9J0KT8BE.localdomain>
2021-08-13 17:49:02 -0400

    refactored appointment tests by adding beforeEach fn

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,28 +1,23 @@
-describe("Navigation", () => {
-  it("should visit root", () => {
-    cy.visit("/");
+describe("Appointments", () => {
+  beforeEach(() => {
+   cy.request("GET", "/api/debug/reset");
+ 
+   cy.visit("/");
+ 
+   cy.contains("Monday");
   });
-
+ 
   it("should book an interview", () => {
-    cy.visit("/");
-    cy.contains("Monday")
-
-    cy.get("[alt=Add]")
-      .first()
-      .click();
-
-    cy.get("[data-testid=student-name-input]")
-      .type("Lydia Miller-Jones");
-  });
-
-  cy.get("[alt='Sylvia Palmer']")
+   cy.get("[alt=Add]")
+    .first()
     .click();
-
-  cy.contains("Save").click();
-
-  cy.request("GET", "/api/debug/reset")
-
-  cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-  cy.contains(".appointment__card--show", "Sylvia Palmer");
-
-});
+ 
+   cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+   cy.get('[alt="Sylvia Palmer"]').click();
+ 
+   cy.contains("Save").click();
+ 
+   cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+   cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+ });
